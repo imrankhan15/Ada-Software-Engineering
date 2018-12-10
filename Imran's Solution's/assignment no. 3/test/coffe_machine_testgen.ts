702aaad675aeb491aa97d37c331410b9e87f1778
imrankhan15 <buetcse110@gmail.com>
2018-12-10 15:00:13 +0100

    change in the coffee_machine_testgen.ts

--- Imran's Solution's/assignment no. 3/test/coffe_machine_testgen.ts
+++ Imran's Solution's/assignment no. 3/test/coffe_machine_testgen.ts
@@ -3,6 +3,102 @@ Context
     with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
     with Ada.Text_IO; use Ada.Text_IO;
 
+***** Test the coffee machine for state 0 and one time 10 cents
+Define Sta: coffee_machine.State;
+       Re: coffee_machine.Reaction;
+       Expected_Reaction: coffee_machine.Reaction := Nothing;
+       Actual_Reaction: coffee_machine.Reaction;
+Prepare       coffee_machine.Initialize(X => Sta);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Ten_Cent,
+                    React => Re);
+
+Test   Actual_Reaction := Re;
+Pass   Expected_Reaction = Actual_Reaction
+
+***** Test the coffee machine for state 0 and one time 20 cents
+Define Sta: coffee_machine.State;
+       Re: coffee_machine.Reaction;
+       Expected_Reaction: coffee_machine.Reaction := Nothing;
+       Actual_Reaction: coffee_machine.Reaction;
+Prepare       coffee_machine.Initialize(X => Sta);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Twenty_Cent,
+                    React => Re);
+
+Test   Actual_Reaction := Re;
+Pass   Expected_Reaction = Actual_Reaction
+
+***** Test the coffee machine for state 1 one time 10 cents
+Define Sta: coffee_machine.State;
+       Re: coffee_machine.Reaction;
+       Expected_Reaction: coffee_machine.Reaction := Nothing;
+       Actual_Reaction: coffee_machine.Reaction;
+Prepare       coffee_machine.Initialize(X => Sta);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Ten_Cent,
+                    React => Re);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Ten_Cent,
+                    React => Re);
+
+Test   Actual_Reaction := Re;
+Pass   Expected_Reaction = Actual_Reaction
+
+***** Test the coffee machine for state 1 one time 20 cents
+Define Sta: coffee_machine.State;
+       Re: coffee_machine.Reaction;
+       Expected_Reaction: coffee_machine.Reaction := Coffee;
+       Actual_Reaction: coffee_machine.Reaction;
+Prepare       coffee_machine.Initialize(X => Sta);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Ten_Cent,
+                    React => Re);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Twenty_Cent,
+                    React => Re);
+
+Test   Actual_Reaction := Re;
+Pass   Expected_Reaction = Actual_Reaction
+
+***** Test the coffee machine for state 2 one time 10 cents
+Define Sta: coffee_machine.State;
+       Re: coffee_machine.Reaction;
+       Expected_Reaction: coffee_machine.Reaction := Coffee;
+       Actual_Reaction: coffee_machine.Reaction;
+Prepare       coffee_machine.Initialize(X => Sta);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Ten_Cent,
+                    React => Re);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Ten_Cent,
+                    React => Re);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Ten_Cent,
+                    React => Re);
+
+Test   Actual_Reaction := Re;
+Pass   Expected_Reaction = Actual_Reaction
+
+***** Test the coffee machine for state 2 one time 20 cents
+Define Sta: coffee_machine.State;
+       Re: coffee_machine.Reaction;
+       Expected_Reaction: coffee_machine.Reaction := Coffee;
+       Actual_Reaction: coffee_machine.Reaction;
+Prepare       coffee_machine.Initialize(X => Sta);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Ten_Cent,
+                    React => Re);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Ten_Cent,
+                    React => Re);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Twenty_Cent,
+                    React => Re);
+
+Test   Actual_Reaction := Re;
+Pass   Expected_Reaction = Actual_Reaction
+
 ***** Test the coffee machine for three times 10 cents
 Define Sta: coffee_machine.State;
        Re: coffee_machine.Reaction;
@@ -22,6 +118,19 @@ Prepare       coffee_machine.Initialize(X => Sta);
 Test   Actual_Reaction := Re;
 Pass   Expected_Reaction = Actual_Reaction
 
+***** Test the coffee machine for no cents and then pressing button
+Define Sta: coffee_machine.State;
+       Re: coffee_machine.Reaction;
+       Expected_Reaction: coffee_machine.Reaction := Nothing;
+       Actual_Reaction: coffee_machine.Reaction;
+Prepare       coffee_machine.Initialize(X => Sta);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Button,
+                    React => Re);
+
+Test   Actual_Reaction := Re;
+Pass   Expected_Reaction = Actual_Reaction
+
 ***** Test the coffee machine for one times 10 cents and then pressing button
 Define Sta: coffee_machine.State;
        Re: coffee_machine.Reaction;
@@ -30,6 +139,23 @@ Define Sta: coffee_machine.State;
 Prepare       coffee_machine.Initialize(X => Sta);
               coffee_machine.X(S     => Sta,
                     Act   => coffee_machine.Ten_Cent,
+                    React => Re);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Button,
+                    React => Re);
+
+
+Test   Actual_Reaction := Re;
+Pass   Expected_Reaction = Actual_Reaction
+
+***** Test the coffee machine for one times 20 cents and then pressing button
+Define Sta: coffee_machine.State;
+       Re: coffee_machine.Reaction;
+       Expected_Reaction: coffee_machine.Reaction := Drop_All_Coins;
+       Actual_Reaction: coffee_machine.Reaction;
+Prepare       coffee_machine.Initialize(X => Sta);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Twenty_Cent,
                     React => Re);
               coffee_machine.X(S     => Sta,
                     Act   => coffee_machine.Button,
@@ -72,3 +198,36 @@ Prepare       coffee_machine.Initialize(X => Sta);
 
 Test   Actual_Reaction := Re;
 Pass   Expected_Reaction = Actual_Reaction
+
+***** Test the coffee machine for all other cases
+Define Sta: coffee_machine.State;
+       Re: coffee_machine.Reaction;
+       Expected_Reaction: coffee_machine.Reaction := Nothing;
+       Actual_Reaction: coffee_machine.Reaction;
+Prepare       coffee_machine.Initialize(X => Sta);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Button,
+                    React => Re);
+
+
+
+Test   Actual_Reaction := Re;
+Pass   Expected_Reaction = Actual_Reaction
+
+***** Test the coffee machine for initializing
+Define Sta: coffee_machine.State;
+       Re: coffee_machine.Reaction;
+       Expected_Reaction: coffee_machine.Reaction := Nothing;
+       Actual_Reaction: coffee_machine.Reaction;
+Prepare       coffee_machine.Initialize(X => Sta);
+              coffee_machine.X(S     => Sta,
+                    Act   => coffee_machine.Button,
+                    React => Re);
+
+
+
+Test   Actual_Reaction := Re;
+Pass   Expected_Reaction = Actual_Reaction
+
+
+
